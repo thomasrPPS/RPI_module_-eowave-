10ba78912e9c5d2af29b7e2f317cf7da15d2718d
Thomas Rio <thomasrio0506@gmail.com>
2022-07-29 16:06:29 +0200

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade
 
 #pd + python libs (20 min)
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install pd -y
 sudo apt-get install python3-pil -y
 sudo apt-get install python3-pip -y
@@ -18,10 +19,10 @@ sudo chmod 666 /dev/ttyAMA0
 
 #enlever "console=serial0, 115200" dans /boot/cmdline.txt et ajouter quiet
 #ajouter ça à la fin de /boot/config.txt et pas de trucs liés au bluethooth :)))
-sudo sed -i 's/console=serial0, 115200//g' /boot/cmdline.txt
-echo "quiet" | sudo tee -a /boot/cmdline.txt
+sudo sed -i 's/console=serial0,115200/ /g' /boot/cmdline.txt
+echo " quiet" | sudo tee /boot/cmdline.txt
 
-sudo sed -i 's/dtoverlay=vc4-kms-v3d//g' /boot/cmdline.txt
+sudo sed -i 's/dtoverlay=vc4-kms-v3d/ /g' /boot/config.txt
 echo "disable_splash=1
 boot_delay=0
 dtparam=i2c_arm=on
@@ -35,24 +36,10 @@ sudo cp /home/eowave/myfiles/multi.pd /root/Pd/externals/
 
 #serveur apache php + nouveau path
 sudo apt install apache2 -y
-sudo apt install php php-mbstring
+sudo apt install php php-mbstring -y
 
 sudo chgrp -R www-data /home/eowave/myfiles/
 sudo chmod -R 2750 /home/eowave/myfiles/
-
-sudo sed -i 's/
-<Directory /var/www>
-        Options Indexes FollowSymLinks
-        AllowOverride None
-        Require all granted
-</Directory>
-/
-<Directory /home/pi/myfiles/html>
-        Options Indexes FollowSymLinks
-        AllowOverride None
-        Require all granted
-</Directory>/g' /etc/apache2/apache2.conf
-
 
 #lancement du systeme au démmarage (fonctionne pas encore)
 echo "[Unit]
@@ -72,7 +59,7 @@ WantedBy=multi-user.target" | sudo tee /etc/systemd/python-pd.service
 sudo systemctl enable python-pd.service
 
 #ensuite setup de la carte son :
-alsamixer -c 0
-amixer -D pulse sset Master 100%
+alsamixer -c 0 
+amixer -D pulse sset Master 100% esc
 
 sudo reboot
